0f34544959a26c2d7904a8fed133e4686471fd03
Arthur07Melo <aarthur00melo@gmail.com>
2023-02-21 14:23:39 -0300

    Adicionado projeto: react calculator

--- src/components/allProjects.tsx
+++ src/components/allProjects.tsx
@@ -1,12 +1,18 @@
 import ProjectCard from "./projectCard"
+import ComingSoon from "../assets/coming-soon.svg"
+import CalculatorScr from "../assets/calculatorScr.svg"
 
 export default function AllProjects(){
     return(
         <div className="my-16 max-w-7xl rounded-md flex flex-wrap justify-center gap-20">
-            <ProjectCard href="#" projectName="PROJECT NAME" />
-            <ProjectCard href="#" projectName="PROJECT NAME" />
-            <ProjectCard href="#" projectName="PROJECT NAME" />
-            <ProjectCard href="#" projectName="PROJECT NAME" />
+            <ProjectCard href="https://calculator-reactproject.vercel.app" projectName="REACT CALCULATOR" image={CalculatorScr} description="
+            Calculadora simples feita em react, utilizando o template de projetos do viteJS. Utilizados também: Html, Css, Javascript." />
+            <ProjectCard href="#" projectName="PROJECT NAME" image={ComingSoon} description="
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique nostrum aliquam quasi numquam tempore vitae, dicta ad exercitationem! Ullam modi recusandae similique provident sint excepturi cumque accusamus perferendis cum dolor." />
+            <ProjectCard href="#" projectName="PROJECT NAME" image={ComingSoon} description="
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique nostrum aliquam quasi numquam tempore vitae, dicta ad exercitationem! Ullam modi recusandae similique provident sint excepturi cumque accusamus perferendis cum dolor." />
+            <ProjectCard href="#" projectName="PROJECT NAME" image={ComingSoon} description="
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique nostrum aliquam quasi numquam tempore vitae, dicta ad exercitationem! Ullam modi recusandae similique provident sint excepturi cumque accusamus perferendis cum dolor." />
         </div>
     )
 }
--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 
 type propsType = {
     href: string,
-    projectName:string
+    projectName:string,
+    image: any,
+    description: string
 }
 
 
@@ -11,13 +13,13 @@ export default function ProjectCard(props:propsType){
     return(
         <div className="flex flex-col justify-center items-center max-w-sm shadow-lg">
             <div className="w-fit rounded-md flex items-center justify-center">
-                <a href={props.href}><Image className="rounded-t-md" src={ComingSoon} width={384} alt="project image" /></a>
+                <a href={props.href}><Image className="rounded-t-md hover:brightness-50" src={props.image} width={384} alt="project image" /></a>
             </div>
 
-            <div className="bg-gray-200 flex flex-col justify-center rounded-md p-3">
+            <div className="bg-gray-200 w-full h-full flex flex-col rounded-md p-3">
                 <h1 className="text-3xl w-fit">{props.projectName}</h1>
                 <h1 className="text-2xl w-fit pt-10">Description</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui iste, delectus totam libero rem ut cumque. Nihil perferendis omnis nemo autem, placeat reprehenderit molestias a nesciunt laboriosam non sapiente vero.</p>
+                <p>{props.description}</p>
             </div>
         </div>
     )
