7359894893e1dd838b56817a58a502b842109247
Arthur07Melo <aarthur00melo@gmail.com>
2023-02-21 16:29:27 -0300

    Adicionado footer.

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer(){
+    return(
+        <div className="text-white font-bold text-lg h-24 bg-black bg-opacity-60 w-full flex justify-center">
+            Portfólio criado com react, nextJS e tailwindCSS, <a className="text-sky-600" href="https://github.com/Arthur07Melo/Portfolio">link do repositório</a>
+        </div>
+    )
+}
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,14 +1,18 @@
 import ProfileCard from "./profileCard";
 import AllProjects from "./allProjects";
+import Footer from "./footer";
 
 
 export default function MainComponent(){
     return (
-        <main className="flex flex-col items-center justify-center bg-gradient-to-tr from-white to-gray-600">
-            <div className="w-full bg-[url('../src/assets/bg-image.svg')] bg-no-repeat bg-cover bg-fixed bg-center flex justify-center">
-                <ProfileCard />
+        <main className="flex flex-col items-center justify-center bg-[url('../src/assets/bg-image.svg')] bg-no-repeat bg-cover bg-fixed bg-center">
+            <ProfileCard />
+
+            <div className="w-full bg-gradient-to-tr from-white to-gray-500 flex justify-center">
+                <AllProjects />
             </div>
-            <AllProjects />
+
+            <Footer />
         </main>
     )
 }
